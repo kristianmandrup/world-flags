3684b325990885f03c6a6025d6517bd36ca54f10
Kristian Mandrup <kmandrup@gmail.com>
2012-04-17 21:57:25 +0200

    bumped version

--- lib/world_flags/rails/engine.rb
+++ lib/world_flags/rails/engine.rb
@@ -1,19 +1,13 @@
 module WorldFlags
   module Rails
-    class Engine < ::Rails::Engine
-        
+    class Engine < ::Rails::Engine        
     	initializer "setup for rails" do
-    		# puts "WorldFlags engine loaded"
     		WorldFlags::Rails::Engine.add_view_ext
       end
 
     	def self.add_view_ext
     		ActionView::Base.send :include, WorldFlags::ViewHelper
     	end
-
-			def self.add_controller_ext
-      	# ActionController::Base.send(:include, WorldFlags::UiControllerExtensions)
-      end	
     end
   end
 end
