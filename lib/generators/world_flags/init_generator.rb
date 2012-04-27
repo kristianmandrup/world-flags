407dd6ecb1928e867be279d430b72ba9c5b55078
Kristian Mandrup <kmandrup@gmail.com>
2012-04-27 09:02:10 +0200

    updated generator - fix

--- lib/generators/world_flags/init_generator.rb
+++ lib/generators/world_flags/init_generator.rb
@@ -1,12 +1,12 @@
 module WorldFlags
   module Generators
     class InitGenerator < Rails::Generators::Base
-      desc "Creates locales initializer"
+      desc "Creates world flags initializer"
 
       source_root File.dirname(__FILE__) + '/templates'
 
       def main_flow
-        template "locale.erb", "config/locales/#{locale}/#{file}"
+        template "world_flags.erb", "config/initializers/world_flags.rb"
       end      
     end
   end
